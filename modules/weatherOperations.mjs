6be104c49e714aa5d5e774ec2172121f4fb6030d
swatiguria <swatiguria2001@gmail.com>
2023-08-09 22:39:01 +0530

    Weather repor fetching API

---
+++ modules/weatherOperations.mjs
@@ -0,0 +1,68 @@
+// import * as data from "./weatherDb.mjs";
+import * as fs from "fs";
+
+let weatherArray = "";
+
+try {
+  let resultOfRead = fs.readFileSync("./weatherDatabase.txt");
+  weatherArray = JSON.parse(resultOfRead);
+} catch (err) {
+  console.log(err);
+}
+let myData = {
+  name: "",
+  tempC: "0",
+  tempF: "0",
+
+  condition: {
+    Text: "",
+    humidity: "0",
+    feelsLikeC: "0",
+    feelsLikeF: "0",
+  },
+};
+
+//adddata
+
+function addData(cityName) {
+  myData.name = cityName;
+  weatherArray.push(myData);
+}
+addData("india");
+console.log(weatherArray);
+
+// updateData
+
+function updateData(cityName, updatedTempC, updatedTempF, text) {
+  let getData = weatherArray.find((o) => o.name === cityName);
+  getData.tempC = updatedTempC;
+  getData.tempF = updatedTempF;
+  getData.condition.Text = text;
+}
+updateData("india", "32.0", "81.0", "windy");
+console.log(weatherArray);
+
+//deletedata
+
+function deleteData(cityName) {
+  let cityIndex = weatherArray.findIndex((o) => o.name === cityName);
+  weatherArray.splice(cityIndex, 1);
+}
+deleteData("india");
+console.log(weatherArray);
+
+//writeData
+function createData() {
+  fs.writeFileSync(
+    "weatherDatabase.txt",
+    JSON.stringify(weatherArray),
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Data Updated!");
+      }
+    }
+  );
+}
+createData();
